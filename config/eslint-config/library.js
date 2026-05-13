4d2c896ba9903416fd08df651e54ba3131831f23
JoaoPe22 <joao.canezin22@gmail.com>
2026-05-13 17:20:44 -0400

    chore: reorganize workspace tooling configs

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,6 +1,6 @@
 /**@type {import('eslint').Linter.Config} */
 const config = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/react'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
